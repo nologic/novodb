f30491ec27f92608236a4645a5ed52fa8ec87bf0
Mikhail Sosonkin <mike@nanotick.net>
2024-11-16 12:08:52 -0500

    removing big libs

--- libs/src/LLDB.framework/Versions/A/Resources/Python/lldb/formatters/objc/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ["Selector", "objc", "Class", "CFArray", "CFBag", "CFBinaryHeap", "CFBitVector", "CFDictionary", "CFString", "NSBundle", "NSData", "NSDate", "NSException", "NSIndexSet", "NSMachPort", "NSNotification", "NSNumber", "NSSet", "NSURL"]
-for x in __all__:
-    __import__('lldb.formatters.objc.'+x)
--- libs/src/LLDB.framework/Versions/A/Resources/Python/lldb/runtime/objc/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ["objc_runtime"]
-for x in __all__:
-    __import__('lldb.runtime.objc.'+x)
